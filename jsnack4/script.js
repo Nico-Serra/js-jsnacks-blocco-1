13ffd1e5d629f246562ec6a10c4bb14b03dc4957
Nico-Serra <nico.serra000@gmail.com>
2024-02-23 12:59:16 +0100

    add snack 4

---
+++ jsnack4/script.js
@@ -0,0 +1,40 @@
+console.log('Hello world');
+
+/*
+In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa
+*/
+
+const partecipants = [
+    'Luca', 'Fabio' , 'Giovanni' , 'Carmine' , 'Mattia' , 'Nicola'
+]
+
+console.log(partecipants);
+
+let access = false
+
+console.log(access);
+
+const userName = prompt('Scrivi qui il tuo nome')
+
+console.log(userName);
+
+for (let i = 0; i < partecipants.length; i++) {
+    const partecipant = partecipants[i];
+    console.log(partecipant);
+
+    if (userName === partecipant) {
+        console.log('Benvenuto al party');
+        access = true
+    }
+    
+}
+
+if (access === true) {
+    console.log('Benvenuto al party');
+    
+}else {
+    console.log('Non puoi entrare');
+}
+
+
+
