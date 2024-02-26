093e6c13eb7698ab5ced8933976262694d50f617
Nico-Serra <nico.serra000@gmail.com>
2024-02-26 10:57:46 +0100

    add jsnack3

---
+++ jsnack3/script.js
@@ -0,0 +1,24 @@
+console.log('hello world');
+
+/*
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+// for (let i = 0; i < 10; i++) {
+//     let element =Number(prompt('Inserisci un numero'))
+//     console.log(element);
+
+//     let som = element + element
+//     console.log(som);
+// }
+
+let i = 0
+while (i < 10) {
+    let element =Number(prompt('Inserisci un numero'))
+    console.log(element);
+
+    let som = element + element
+    console.log(som);
+    i++
+}
