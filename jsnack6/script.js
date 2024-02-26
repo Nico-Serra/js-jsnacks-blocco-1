5ef89c89a89f01949927d59379c22e960bd5a600
Nico-Serra <nico.serra000@gmail.com>
2024-02-26 12:26:31 +0100

    add loop for and while

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -5,8 +5,29 @@ Stampa il cubo dei primi N numeri,
 dove N è un numero indicato dall’utente.
 */
 
-let n = Number(prompt('inserisci un numero'))
+// let n = Number(prompt('inserisci un numero'))
+// console.log(n);
+
+// const cube = n*n*n
+// console.log(cube);
+
+
+// for (let i = 0; i < 3; i++) {
+//     let n = Number(prompt('inserisci un numero'))
+// console.log(n);
+
+// const cube = n*n*n
+// console.log(cube);
+    
+// }
+
+
+let i = 0;
+while (i < 3) {
+    let n = Number(prompt('inserisci un numero'))
 console.log(n);
 
 const cube = n*n*n
 console.log(cube);
+    i++
+}
