1422430b2802273ffcdba84a612866e5ebf844ea
Nico-Serra <nico.serra000@gmail.com>
2024-02-26 12:19:22 +0100

    add jsnack6

---
+++ jsnack6/script.js
@@ -0,0 +1,12 @@
+console.log('hello world');
+
+/*
+Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall’utente.
+*/
+
+let n = Number(prompt('inserisci un numero'))
+console.log(n);
+
+const cube = n*n*n
+console.log(cube);
